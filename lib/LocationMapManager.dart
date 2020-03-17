191aa0b1a4343c3d6c306a86376477253f58fa3d
Winston <winstonyeo99@yahoo.com>
2020-03-17 21:24:06 +0800

    🚧 Add method to create markers from json data

--- lib/LocationMapManager.dart
+++ lib/LocationMapManager.dart
@@ -1,11 +1,34 @@
-import 'dart:math';
-
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:taxi_availability/Services/TaxiAvailability.dart';
-import 'package:taxi_availability/json/Response.dart';
+
+Set<Marker> markersFromList(List<List<double>> latlong) {
+  Set<Marker> _markers = Set();
+  // for (List<double> coordinate in latlong) {
+  // print("longitudinal: " + coordinate[0].toString());
+  // print("Latitudinal: " + coordinate[1].toString());
+  var coordinate = latlong;
+  for (int i = 0; i < 10; i++) {
+    _markers.add(
+      Marker(
+        markerId: MarkerId("hello"),
+        position: LatLng(
+          coordinate[i][0],
+          coordinate[i][1],
+        ),
+        infoWindow:
+            InfoWindow(title: "InfoWindow", snippet: 'This is a snipper'),
+        onTap: () {
+          print("marker tapped");
+        },
+      ),
+    );
+  }
+  print(_markers);
+  return _markers;
+}
 
 class LocationMapManager {
-  final int _delay = 5;
+  final int _delay = 15;
   TaxiAvailability taxi;
 
   LocationMapManager(this.taxi);
@@ -13,31 +36,29 @@ class LocationMapManager {
   Stream<Set<Marker>> get taxiMarkers async* {
     while (true) {
       await Future.delayed(Duration(seconds: _delay));
-      CabDetails details = await taxi.markers(DateTime.now());
-      print(details.cabInfo);
-      CabCoordinates coordinates = details?.cabCoordinates;
-      print(coordinates);
-      List<List<int>> latlong = coordinates?.coordinates;
-      print(latlong);
-      yield _markersFromList(latlong);
+      List<List<double>> latlong = await taxi.markers(DateTime.now());
+      // Set<Marker> toYield = await compute(markersFromList, latlong);
+      Set<Marker> _markers = Set();
+      int length = latlong.length;
+      for (int i = 0; i < length; i++) {
+        // for (List<double> coordinate in latlong) {
+        print("adding marker");
+        _markers.add(
+          Marker(
+            markerId: MarkerId("Marker$i"),
+            position: LatLng(
+              latlong[i][1],
+              latlong[i][0],
+            ),
+            // infoWindow:
+            //     InfoWindow(title: "InfoWindow", snippet: 'This is a snipper'),
+            // onTap: () {
+            //   print("marker tapped");
+            // },
+          ),
+        );
+        yield _markers;
+      }
     }
-  }
-
-  Set<Marker> _markersFromList(List<List<int>> latlong) {
-    print("making markers");
-    Random generator = Random();
-    Set<Marker> _markers = Set();
-    _markers.add(Marker(
-      markerId: MarkerId("hello"),
-      position: LatLng(
-        1 + generator.nextDouble(),
-        103 + generator.nextDouble(),
-      ),
-      infoWindow: InfoWindow(title: "InfoWindow", snippet: 'This is a snipper'),
-      onTap: () {
-        print("marker tapped");
-      },
-    ));
-    return _markers;
   }
 }
